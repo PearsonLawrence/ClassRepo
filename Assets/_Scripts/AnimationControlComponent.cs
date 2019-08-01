8d5e9165565fd473531591cdc3ec61b9671cf1f5
Pearson Lawrence <s179130@students.aie.edu.au>
2019-07-31 18:28:23 -0700

    init

---
+++ Assets/_Scripts/AnimationControlComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControlComponent : MonoBehaviour {
+
+    public Animator Anim;
+    public Rigidbody RB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+                                       
+        Vector3 LocalVelocity = transform.InverseTransformDirection(RB.velocity);
+
+        Anim.SetFloat("HorzSpeed", LocalVelocity.x);
+        Anim.SetFloat("VertSpeed", LocalVelocity.z);
+
+    }
+}
